36e19cda995ba743f2f58e9423b00b4207565696
bhola mahto <bholamahto255@gmail.com>
2024-10-15 18:29:14 +0530

    created the login page

--- src/app/client/about/page.tsx
+++ src/app/client/about/page.tsx
@@ -1,9 +1,8 @@
-
 import React from "react";
 import Header from "@/components/common/Header";
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
-import Image from "next/image"; 
+import Image from "next/image";
 
 import about_img from "../../../assets/imgs/about_img.png";
 import mission_1 from "../../../assets/imgs/banner_3.png";
@@ -16,13 +15,11 @@ import core_6 from "../../../assets/imgs/core_6.png";
 
 interface Core {
   id: number;
-  src:StaticImageData;
+  src: StaticImageData;
   alt: string;
   heading: string;
   content: string;
 }
- 
-
 
 const About: React.FC = () => {
   const cores: Core[] = [
@@ -31,21 +28,24 @@ const About: React.FC = () => {
       src: core_4,
       alt: "Core Value 1",
       heading: "Core Value 1",
-      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
     {
       id: 2,
       src: core_5,
       alt: "Core Value 2",
       heading: "Core Value 2",
-      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
     {
       id: 3,
       src: core_6,
       alt: "Core Value 3",
       heading: "Core Value 3",
-      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     },
   ];
 
@@ -55,143 +55,145 @@ const About: React.FC = () => {
       <Navbar />
 
       <div className=" w-full  h-[300px] mt-10  justify-center   hidden md:flex ">
-  <div className="w-full relative  mx-4"> 
-    <div
-      className="absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center rounded-md"
-      style={{
-        backgroundImage: `url(${about_img.src})`,
-      }}
-    >
-      <div className="flex flex-col justify-center items-center w-full h-full gap-5 bg-black bg-opacity-50 p-4 md:p-6 lg:p-8">
-        <h1 className="text-3xl font-bold">About Us</h1>
-        <p className="text-center max-w-lg">
-          At GlamGuider, we are dedicated to helping you discover the latest trends in beauty and fashion. Our curated selection of products and expert tips empower you to express your unique style with confidence.
-        </p>
+        <div className="w-full relative  mx-4">
+          <div
+            className="absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center rounded-md"
+            style={{
+              backgroundImage: `url(${about_img.src})`,
+            }}
+          >
+            <div className="flex flex-col justify-center items-center w-full h-full gap-5 bg-black bg-opacity-50 p-4 md:p-6 lg:p-8">
+              <h1 className="text-3xl font-bold">About Us</h1>
+              <p className="text-center max-w-lg">
+                At GlamGuider, we are dedicated to helping you discover the
+                latest trends in beauty and fashion. Our curated selection of
+                products and expert tips empower you to express your unique
+                style with confidence.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
 
-
-<div className=" w-full h-[300px]  mt-10 flex justify-center  md:hidden"> 
-  <div className="w-full relative  flex justify-center mx-4"> 
-    <div
-      className="absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center rounded-md" 
-      style={{
-        backgroundImage: `url(${about_img.src})`,
-      }}
-    >
-      <div className="flex flex-col justify-center items-center w-full h-full gap-5 bg-black bg-opacity-50 p-4 md:p-6">
-        <h1 className="text-2xl font-bold">About Us</h1> 
-        <p className="text-center max-w-md">
-          At GlamGuider, we are dedicated to helping you discover the latest trends in beauty and fashion. Our curated selection of products and expert tips empower you to express your unique style with confidence.
-        </p>
+      <div className=" w-full h-[300px]  mt-10 flex justify-center  md:hidden">
+        <div className="w-full relative  flex justify-center mx-4">
+          <div
+            className="absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center rounded-md"
+            style={{
+              backgroundImage: `url(${about_img.src})`,
+            }}
+          >
+            <div className="flex flex-col justify-center items-center w-full h-full gap-5 bg-black bg-opacity-50 p-4 md:p-6">
+              <h1 className="text-2xl font-bold">About Us</h1>
+              <p className="text-center max-w-md">
+                At GlamGuider, we are dedicated to helping you discover the
+                latest trends in beauty and fashion. Our curated selection of
+                products and expert tips empower you to express your unique
+                style with confidence.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
 
+      <div className="my-10 ">
+        <h1 className="text-center text-black text-5xl font-normal">
+          Our Missions
+        </h1>
 
+        <div className="flex flex-col md:flex-row justify-center items-center mt-10 gap-10 mx-4 md:mx-8 lg:mx-36">
+          <div className="w-full md:w-[50%] lg:w-[640px] h-auto">
+            <Image
+              src={mission_1}
+              alt="Mission"
+              className="w-full h-auto"
+              layout="responsive"
+              width={640}
+              height={350}
+            />
+          </div>
+          <div className="flex flex-col justify-center items-start space-y-5 w-full md:w-[50%] lg:w-[640px]">
+            <div className="flex items-center gap-3">
+              <Image src={symbol} alt="Symbol" width={40} height={40} />
+              <p>
+                At GlamGuider, we believe in empowering individuals through
+                beauty and fashion.
+              </p>
+            </div>
+            <div className="flex items-center gap-3">
+              <Image src={symbol} alt="Symbol" width={40} height={40} />
+              <p>
+                We strive to provide expert advice and insights that inspire
+                confidence in every customer.
+              </p>
+            </div>
+            <div className="flex items-center gap-3">
+              <Image src={symbol} alt="Symbol" width={40} height={40} />
+              <p>
+                Our commitment is to bring you the latest trends while ensuring
+                a personalized shopping experience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <div className="w-full ">
+        <div className="relative mb-5 hidden md:block  mx-4">
+          <Image
+            src={who}
+            alt="Who We Are"
+            className="w-full h-auto object-cover"
+            layout="responsive"
+          />
+          <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
+            <div className="bg-[#EDF5F5]  p-10 text-center space-y-6">
+              <p className="text-lg">
+                We are dedicated to providing a seamless shopping experience
+                that prioritizes convenience, inclusivity, and customer
+                satisfaction.
+              </p>
+              <p className="text-lg">
+                By connecting our community with top-rated products, we inspire
+                confidence and celebrate the beauty in diversity for everyone.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
 
-<div className="my-10 ">
-        <h1 className="text-center text-black text-5xl font-normal">Our Missions</h1>
-    
+      <div className="w-full">
+        <div className="relative  mx-4 md:hidden">
+          <Image
+            src={who}
+            alt="Who We Are"
+            className="w-full h-auto object-cover"
+            layout="responsive"
+          />
+          <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-center items-center bg-[#EDF5F5]  text-center rounded-md ">
+            <p className="text-sm w-full h-[50%] ">
+              We are dedicated to providing a seamless shopping experience that
+              prioritizes convenience, inclusivity, and customer satisfaction.
+            </p>
+          </div>
+        </div>
+      </div>
 
-
-
-<div className="flex flex-col md:flex-row justify-center items-center mt-10 gap-10 mx-4 md:mx-8 lg:mx-36">
-  <div className="w-full md:w-[50%] lg:w-[640px] h-auto">
-    <Image
-      src={mission_1}
-      alt="Mission"
-      className="w-full h-auto"
-      layout="responsive" 
-      width={640} 
-      height={350} 
-    />
-  </div>
-  <div className="flex flex-col justify-center items-start space-y-5 w-full md:w-[50%] lg:w-[640px]">
-    <div className="flex items-center gap-3">
-      <Image src={symbol} alt="Symbol" width={40} height={40} />
-      <p>At GlamGuider, we believe in empowering individuals through beauty and fashion.</p>
-    </div>
-    <div className="flex items-center gap-3">
-      <Image src={symbol} alt="Symbol" width={40} height={40} />
-      <p>We strive to provide expert advice and insights that inspire confidence in every customer.</p>
-    </div>
-    <div className="flex items-center gap-3">
-      <Image src={symbol} alt="Symbol" width={40} height={40} />
-      <p>Our commitment is to bring you the latest trends while ensuring a personalized shopping experience.</p>
-    </div>
-  </div>
-</div>
-
- </div>
-
-   
-
-<div className="w-full ">
-<div className="relative mb-5 hidden md:block  mx-4"> 
-  <Image
-    src={who}
-    alt="Who We Are"
-    className="w-full h-auto object-cover"
-    layout="responsive"
-  
-  />
-  <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
-    <div className="bg-[#EDF5F5]  p-10 text-center space-y-6">
-      <p className="text-lg">
-        We are dedicated to providing a seamless shopping experience that prioritizes convenience, inclusivity, and customer satisfaction.
-      </p>
-      <p className="text-lg">
-        By connecting our community with top-rated products, we inspire confidence and celebrate the beauty in diversity for everyone.
-      </p>
-    </div>
-  </div>
-</div>
-</div>
-
-
-
-
-
-
-
-<div className="w-full">
-
-<div className="relative  mx-4 md:hidden">
-  <Image
-    src={who}
-    alt="Who We Are"
-    className="w-full h-auto object-cover"
-    layout="responsive"
-  
-  />
-  <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-center items-center bg-[#EDF5F5]  text-center rounded-md ">
-    <p className="text-sm w-full h-[50%] ">
-      We are dedicated to providing a seamless shopping experience that prioritizes convenience, inclusivity, and customer satisfaction.
-    </p>
-
-  </div>
-</div>
-
-</div>
-
-
-
-
-
-
-
-   
       <div className="px-4 md:px-8 lg:px-16">
         <h1 className="text-center text-5xl">Core Values</h1>
         <div className="flex flex-col md:flex-row justify-center items-center gap-5 mt-10">
           {cores.map((core) => (
-            <div key={core.id} className="bg-[#EDF5F3] w-full md:w-1/3 h-auto rounded-lg flex flex-col items-center justify-center p-5 m-2 shadow">
-              <Image src={core.src} alt={core.alt} width={96} height={96} className="mb-2" />
+            <div
+              key={core.id}
+              className="bg-[#EDF5F3] w-full md:w-1/3 h-auto rounded-lg flex flex-col items-center justify-center p-5 m-2 shadow"
+            >
+              <Image
+                src={core.src}
+                alt={core.alt}
+                width={96}
+                height={96}
+                className="mb-2"
+              />
               <h2 className="text-xl font-semibold">{core.heading}</h2>
               <p className="text-center">{core.content}</p>
             </div>
---
+++ src/app/client/login/page.tsx
@@ -0,0 +1,78 @@
+
+
+
+
+"use client";
+
+import { useState, ChangeEvent, FormEvent } from "react";
+import Image from "next/image";
+import loginImage from "../../../assets/imgs/login_1.png"; 
+
+const Login: React.FC = () => {
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("Phone Number is:", phoneNumber);
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen ">
+      <div className="flex flex-col md:flex-row gap-5 justify-center items-center w-full  p-8 rounded-lg shadow-md">
+        <div className="flex-shrink-0 w-full md:w-1/2 text-center">
+          <Image
+            src={loginImage}
+            alt="Login"
+            className="max-h-[700px] px-5"
+          />
+        </div>
+
+        <div className="flex-1 bg-[#EDF5F5] p-8 rounded-lg shadow-md w-full md:w-1/2">
+          <h1 className="text-center text-2xl font-bold">Hii, Welcome Back</h1>
+          <p className="text-center text-lg mt-2">
+            Please fill your details to access your existing account
+          </p>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <div className="w-full my-5">
+                <label className="text-lg" htmlFor="phone">
+                  Phone Number
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  value={phoneNumber}
+                  onChange={handleInputChange}
+                  className="w-full h-[52px] p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#21747C] mt-2"
+                  placeholder="Enter your phone number"
+                  required
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full h-[54px] text-center bg-[#21747C] text-white rounded-lg mx-auto mt-4"
+              >
+                Continue
+              </button>
+            </form>
+            <div className="text-center text-lg my-5">
+              <span className="text-sm text-gray-600">or login with</span>
+            </div>
+            <button className="w-full h-[54px] text-center bg-blue-600 text-white rounded-lg mx-auto">
+              Login with Google
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
+
